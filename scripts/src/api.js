6361c31722e1fe2d20eb9ac21f6fb08162ae2f27
LuKa44x <lucavicari44@gmail.com>
2025-08-02 17:01:11 +0200

    Converted from promise with .then to async & await
    Changed the fetch in api.js file to async function with await fetch.

--- scripts/src/api.js
+++ scripts/src/api.js
@@ -1,13 +1,13 @@
 //Api Manager
 import { createTable } from "./domManager.js";
 
-export function getWeatherData(){
-    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Illertisen?unitGroup=metric&key=3LME86FB7QN4FEL5SN6GLAAJM&contentType=json", { mode: 'cors' })
-.then(response => {
-    return response.json();
-})
-.then(function (response){
-    createTable(response.days);
-    console.log(response);
-});
+export async function getWeatherData() {
+    try {
+        const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Illerstisen?unitGroup=metric&key=3LME86FB7QN4FEL5SN6GLAAJM&contentType=json");
+        const data = await response.json();
+        createTable(data.days);
+        console.log(data);
+    } catch (err) {
+        alert("Fetch error: ", err);
+    }
 }
